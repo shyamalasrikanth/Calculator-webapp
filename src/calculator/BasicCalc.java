6374387b69b0abe6006cf5905c34bf616acd8afb
HOME <HOME@192.168.0.5>
2017-06-07 11:43:59 -0700

    the program code for BasicCalc.java

--- src/calculator/BasicCalc.java
+++ src/calculator/BasicCalc.java
@@ -3,10 +3,47 @@ package calculator;
 
 public class BasicCalc
 {
-	// include variables a, b, c here
+	double a,b,c;              // include variables a, b, c here
 	 
-	//include getters and setters method to access variables
+	                            //include getters and setters method to access variables
 	
-	//include methods for operations
+	                               //include methods for operations
 	
+
+
+public double getA() {
+	return a;
+}
+public void setA(double a) {
+	this.a = a;
+}
+public double getB() {
+	return b;
+}
+public void setB(double b) {
+	this.b = b;
+}
+
+
+
+double add(){
+	c=a+b;
+	return(c);}
+
+double subtract(){
+	c=a-b;
+	return(c);}
+double multiply(){
+	c=a*b;
+	return(c);}
+double divide(){
+	c=a/b;
+	return(c);}
+
+
+
+
+
+
+
 }
